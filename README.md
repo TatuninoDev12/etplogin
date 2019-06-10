acca26ab0bee71afffb68977338cae95bac6e5fe
EmirTP <emirtp8@gmail.com>
2019-06-10 16:54:27 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# etplogin
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/etplogin)
---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './Login';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, LoginComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
